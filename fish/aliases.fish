9ec387378e0d749df56284d2008596ff0f03d7a1
Todd Makinster <todd.makinster@gmail.com>
2019-03-04 11:01:09 -0600

    Start with the fish config

---
+++ fish/aliases.fish
@@ -0,0 +1,2 @@
+alias gits="git status -sb"
+
---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+. ~/.config/fish/aliases.fish
+
+set -gx LC_ALL en_US.UTF-8
+set -gx LANG en_US.UTF-8
+
+set -gx EDITOR vim
+
+set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+
