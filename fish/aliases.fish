820bad463af8137da364963701e9a1f9bc68ac11
Todd Makinster <53427+kinsteronline@users.noreply.github.com>
2020-10-28 11:38:04 -0500

    Update aliases.fish
    docker-compose shortcutting

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,3 +3,15 @@ alias gitd="git diff"
 alias gs="git status -sb"
 alias gits="git status -sb"
 
+alias dcu="docker-compose up --build"
+alias dcub="docker-compose up --build -d"
+alias dcup="docker-compose up -d"
+
+alias dcdown="docker-compose down"
+alias dcdv="docker-compose down --volumes"
+
+alias dcl="docker-compose logs"
+alias dcr="docker-compose run"
+
+alias dpr="docker system prune"
+alias dprune="docker system prune"
